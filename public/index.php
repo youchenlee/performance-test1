b3ea3fda08728681d4a2c7c255cabfcc791ef2cd
Youchen Lee <copyleft@utcr.org>
2017-04-07 17:49:40 +0800

    test2

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/test', 'helloWorldHandler');
+    $r->addRoute('GET', '/test2', 'helloWorldHandler2');
 });
 
 // Fetch method and URI from somewhere
@@ -34,6 +35,15 @@ function helloWorldHandler() {
     );
     $response->prepare($request);
     $response->send();
+}
+
+
+function helloWorldHandler2() {
+    $request_body = file_get_contents('php://input');
+    http_response_code(200);
+    header('content-type: text/html');
+    echo "Hello World!";
+    exit();
 }
 
 switch ($routeInfo[0]) {
